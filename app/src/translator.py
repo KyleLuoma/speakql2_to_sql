a9b974fd9089d597e3c82c1e8d47a9853af8834f
kyleluoma <kyle@jp8.dev>
2022-01-12 15:57:14 -0800

    updated table_select_agg_rules to include only queryOrderSpecification

--- app/src/translator.py
+++ app/src/translator.py
@@ -2,7 +2,7 @@ from .speakql_translator.SpeakQlParseCaller import *
 from .speakql_translator.SpeakQlTree import *
 from .speakql_translator.speakql_to_sql import *
 
-parse_engine = JavaSpeakQlParseEngine("gen_groupyby_delim")
+parse_engine = JavaSpeakQlParseEngine("gen_queryorderspec")
 parse_caller = SpeakQlParseCaller(parse_engine)
 
 def run_test_query_translation(test_query):
