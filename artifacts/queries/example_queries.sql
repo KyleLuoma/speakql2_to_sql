f488cf2f62122c3a7fd986af2ccc7b4bbb084d4a
kyleluoma <kyle@jp8.dev>
2022-02-14 22:13:21 -0800

    example query file complete

--- artifacts/queries/example_queries.sql
+++ artifacts/queries/example_queries.sql
@@ -39,6 +39,7 @@ SELECT AVG(AREA)
 FROM ROOM
 
 
+
 --Query objective: Look up the department name for department code 'CSE'
 --Demonstrates: synonym and reordering features together
 -- with a where clause (Similar to Q2)
@@ -181,6 +182,7 @@ FROM COURSEOFFERING
 GROUP BY ENDTIME
 
 
+
 --Query objective: Using the room table, find the total number of wheelchair spaces and the average square feet per classroom for
 --classrooms with at least one wheelchair space.
 --Group by each building and building floor. No need for building name, building ID is sufficient.
@@ -208,7 +210,6 @@ SELECT BUILDINGID, FLOOR, COUNT(WHEELCHAIRSPACES), SUM(CAPACITY), AVG(AREA)
 FROM ROOM
 WHERE WHEELCHAIRSPACES > 0
 GROUP BY BUILDINGID, FLOOR
-
 
 
 
@@ -243,7 +244,7 @@ and then
 order by facultyname
 
 
---Translated SQL:
+--Translated SQL (options 1 and 2):
 SELECT COURSEOFFERING.FACULTYNAME, COURSEOFFERING.STARTTIME, COURSE.TITLE, COURSE.UNITS
 FROM COURSEOFFERING
 JOIN COURSE ON COURSE.ID = COURSEOFFERING.COURSEID
@@ -272,6 +273,7 @@ ORDER BY FACULTYNAME
 
 --UNBUNDLED SUBQUERY 
 
+--SpeakQl Query Option 1:
 join department with course on department.id = course.deptid
 and then
 join course with courseoffering on course.id = courseoffering.courseid
@@ -288,6 +290,24 @@ from term get year and termperiod where year >= 2021
 and then
 group by automatic order by year
 
+--SpeakQl Query option 2:
+join the department table with the course table on department.id = course.deptid
+and then
+join the course table with courseoffering on course.id = courseoffering.courseid
+and then
+join the term table with the courseoffering table on term.id = courseoffering.termid
+and then
+get departmentname from the department table
+and then
+find the count of (id) in the courseoffering table
+and then
+get no columns from the course table where units > 4
+and then
+from the term table find year and termperiod where year >= 2021
+and then
+group by automatic
+order by year
+
 
 --Translated SQL:
 SELECT DEPARTMENT.DEPARTMENTNAME, COUNT(COURSEOFFERING.ID), TERM.YEAR, TERM.TERMPERIOD
@@ -297,6 +317,25 @@ JOIN COURSEOFFERING ON COURSE.ID = COURSEOFFERING.COURSEID
 JOIN TERM ON TERM.ID = COURSEOFFERING.TERMID
 WHERE(DEPARTMENT.ID = "CSE") AND(TERM.YEAR = 2022 AND TERM.TERMPERIOD = "SPRING")
 GROUP BY DEPARTMENT.DEPARTMENTNAME, TERM.YEAR, TERM.TERMPERIOD
+
+
+
+--Query objective: Fetch all fields for the rooms that have 2 wheelchair spaces, 
+--or have a seating capacity greater than 40, or that are on the first floor. 
+--Get only the first 10 records. (Similar to Q10)
+--Demonstrates: Application of multiple OR predicates to a single table
+
+--SpeakQl Query
+in the room table
+find *
+where wheelchairspaces = 2 or capacity > 40 or floor = 1
+limit 10
+
+--Translated SQL:
+SELECT *
+FROM ROOM
+WHERE WHEELCHAIRSPACES = 2 OR CAPACITY > 40 OR FLOOR = 1 LIMIT 10
+
 
 
 --Query objective: Make a list of buildings and the total number of classrooms in each building. 
@@ -316,3 +355,114 @@ SELECT BUILDING.BUILDINGNAME, BUILDING.BUILDINGNUMBER, COUNT(ROOM.ID)
 FROM BUILDING, ROOM
 WHERE(ROOM.BUILDINGID = BUILDING.ID)
 GROUP BY BUILDING.BUILDINGNAME, BUILDING.BUILDINGNUMBER
+
+
+
+--Query objective: Display the building name, building number, room number, course title, and start time for courses offered by the CSE department in Fall of 2021 
+--sorted by building number (Similar to Q12)
+--Demonstrates: Join of five different tables with selections across four and a where condition applied to a table without columns in the query.
+
+--SpeakQl Query option 1 (All joins first then selections and projections):
+join the building table with the room table on building.id = room.buildingid
+and then
+join the room table with the courseoffering table on room.id = courseoffering.roomid
+and then
+join the term table with the courseoffering table on term.id = courseoffering.termid
+and then
+join the course table with the courseoffering table on course.id = courseoffering.courseid
+and then
+from the building table display buildingname and buildingnumber
+and then
+from the room table show me roomNumber
+and then
+from the course table show me title where deptid = "cse"
+and then
+from the courseoffering table get ondays and starttime
+and then
+get no columns from the term table where year = 2021 and termperiod = "fall"
+and then
+order by title
+
+--SpeakQl Query option 2a (Alternate between joins and selections and projections):
+get buildingname and buildingnumber from the building table
+and then
+join the building table with the room table on room.buildingid = building.id
+and then
+get roomnumber from the room table
+and then
+join the room table with the courseoffering table on room.id = courseoffering.roomid
+and then
+get ondays and starttime from the courseoffering table
+and then
+join the courseoffering table with the term table on term.id = courseoffering.termid
+and then
+from term where year = 2021 and termperiod = "fall" display no columns
+and then
+join the course table with the courseoffering table on course.id = courseoffering.courseid
+and then 
+get title from the course table where deptid = "cse"
+and then
+order by title
+
+--SpeakQl query option 2b (Less verbose, but same order)
+get buildingname and buildingnumber from building
+and then
+join building with room on room.buildingid = building.id
+and then
+from table room get roomnumber
+and then
+join room with courseoffering on room.id = courseoffering.roomid
+and then
+from courseoffering show me ondays and starttime
+and then
+join courseoffering with term on term.id = courseoffering.termid
+and then
+display no columns from term where year = 2021 and termperiod = "fall"
+and then
+join course with courseoffering on course.id = courseoffering.courseid
+and then
+display title from course where deptid = "cse"
+and then
+order by title
+
+--SpeakQl query option 3 (No joins, only where conditions)
+get buildingname and buildingnumber from the building table
+and then
+get roomnumber from the room table where room.buildingid = building.id
+and then
+get ondays and starttime from the courseoffering table where courseoffering.roomid = room.id
+and then
+get no columns from the term table where year = 2021 and termperiod = "fall" and term.id = courseoffering.termid
+and then
+get title from course where deptid = "cse" and course.id = courseoffering.courseid
+and then
+order by title
+
+--Translated SQL (option 1):
+SELECT BUILDING.BUILDINGNAME, BUILDING.BUILDINGNUMBER, ROOM.ROOMNUMBER, COURSE.TITLE, COURSEOFFERING.ONDAYS, COURSEOFFERING.STARTTIME
+FROM BUILDING
+JOIN ROOM ON BUILDING.ID = ROOM.BUILDINGID 
+JOIN COURSEOFFERING ON ROOM.ID = COURSEOFFERING.ROOMID 
+JOIN TERM ON TERM.ID = COURSEOFFERING.TERMID 
+JOIN COURSE ON COURSE.ID = COURSEOFFERING.COURSEID
+WHERE(COURSE.DEPTID = "CSE") AND(TERM.YEAR = 2021 AND TERM.TERMPERIOD = "FALL") 
+ORDER BY TITLE
+
+--Translated SQL (option 2):
+SELECT BUILDING.BUILDINGNAME, BUILDING.BUILDINGNUMBER, ROOM.ROOMNUMBER, COURSEOFFERING.ONDAYS, COURSEOFFERING.STARTTIME, COURSE.TITLE
+FROM BUILDING
+JOIN ROOM ON ROOM.BUILDINGID = BUILDING.ID 
+JOIN COURSEOFFERING ON ROOM.ID = COURSEOFFERING.ROOMID 
+JOIN TERM ON TERM.ID = COURSEOFFERING.TERMID 
+JOIN COURSE ON COURSE.ID = COURSEOFFERING.COURSEID
+WHERE(TERM.YEAR = 2021 AND TERM.TERMPERIOD = "FALL") AND(COURSE.DEPTID = "CSE") 
+ORDER BY TITLE
+
+--Translated SQL (option 3):
+SELECT BUILDING.BUILDINGNAME, BUILDING.BUILDINGNUMBER, ROOM.ROOMNUMBER, COURSEOFFERING.ONDAYS, COURSEOFFERING.STARTTIME, COURSE.TITLE
+FROM BUILDING, ROOM, COURSEOFFERING, TERM, COURSE
+WHERE(ROOM.BUILDINGID = BUILDING.ID) 
+    AND(COURSEOFFERING.ROOMID = ROOM.ID) 
+    AND(TERM.YEAR = 2021 AND TERM.TERMPERIOD = "FALL" AND TERM.ID = COURSEOFFERING.TERMID) 
+    AND(COURSE.DEPTID = "CSE" AND COURSE.ID = COURSEOFFERING.COURSEID) 
+ORDER BY TITLE
